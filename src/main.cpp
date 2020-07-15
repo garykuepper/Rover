b49f1eee860517ff975080660788e7b0e2fb45ea
garykuepper <65058102+garykuepper@users.noreply.github.com>
2020-07-14 17:09:18 -0700

    FirstDev Added SimpleTimer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "servoshock_PS4.h"
+#include <SimpleTimer.h>
 
 const int slaveSelect = 10;
 Servoshock Servoshock1(slaveSelect);  //create instance of Servoshock
+
+void update();
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,14 +20,15 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // dude
   //
-  // Servoshock1.Update();  // update every 20 ms or 100Hz
-  //
+  update();
+	
+}
+
+void update() {
+  //Servoshock1.Update();  // update every 20 ms or 100Hz
   //Servoshock1.inPacket.lStickX;	
 	//Servoshock1.inPacket.lStickY;	
 	//Servoshock1.inPacket.rStickX;	
 	//Servoshock1.inPacket.rStickY;
-	
 }
