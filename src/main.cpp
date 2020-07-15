db4838d9a45eb9f06613d07e98288eb54ee8a4c1
garykuepper <65058102+garykuepper@users.noreply.github.com>
2020-07-14 17:58:56 -0700

    add motor function

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,76 @@
 #include <SimpleTimer.h>
 
 const int slaveSelect = 10;
+const int leftMotorPin = 9;
+const int leftMotorA = 8;
+const int leftMotorB = 7;
+const int rightMotorPin = 3;
+const int rightMotorA = 4;
+const int rightMotorB = 2;
+int leftSpeed = 0;
+int rightSpeed = 0;
 Servoshock Servoshock1(slaveSelect);  //create instance of Servoshock
+SimpleTimer timer;
 
-void update();
+void servoshockUpdate();
+void motor(int stickIn, int &out);
 
 void setup() {
   // put your setup code here, to run once:
   digitalWrite(slaveSelect, HIGH);
+  pinMode(leftMotorPin, OUTPUT);
+  pinMode(leftMotorA, OUTPUT);
+  pinMode(leftMotorB, OUTPUT);
+  pinMode(rightMotorPin, OUTPUT);
+  pinMode(rightMotorA, OUTPUT);
+  pinMode(rightMotorB, OUTPUT);
+  motor(127, leftMotorPin, leftMotorA, leftMotorB);
+  motor(127, rightMotorPin, rightMotorA, rightMotorB);
+  
 	SPI.setDataMode(SPI_MODE0);
 	SPI.setClockDivider(SPI_CLOCK_DIV16);
 	SPI.setBitOrder(MSBFIRST);
 	SPI.begin();
   Serial.begin(115200);  //initialize serial monitor
-
+  timer.setInterval(20, servoshockUpdate);
 }
 
 void loop() {
   //
-  update();
+  timer.run();
 	
 }
 
-void update() {
-  //Servoshock1.Update();  // update every 20 ms or 100Hz
+void servoshockUpdate() {
+  Servoshock1.Update();  // update every 20 ms or 100Hz
+  motor(Servoshock1.inPacket.lStickY, leftMotorPin, leftMotorA, leftMotorB);
+  motor(Servoshock1.inPacket.rStickY, rightMotorPin, rightMotorA, rightMotorB);
   //Servoshock1.inPacket.lStickX;	
 	//Servoshock1.inPacket.lStickY;	
 	//Servoshock1.inPacket.rStickX;	
 	//Servoshock1.inPacket.rStickY;
+}
+
+void motor(int stickIn, int speedPin, int A, int B) {
+  int deadBand = 5;
+  // fwd
+  if (stickIn > 127 + deadBand) {
+    digitalWrite(A, LOW);
+    digitalWrite(B, HIGH);
+    analogWrite(speedPin, map(stickIn, 127, 255, 0, 255));
+  }
+  //reverse
+  else if (stickIn < 127 - deadBand) {
+    digitalWrite(A, HIGH);
+    digitalWrite(B, LOW);
+    analogWrite(speedPin, map(stickIn, 127, 0, 0, 255));
+  }
+  //stop
+  else {
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+    analogWrite(speedPin, 0);
+  }
+  
+
 }
